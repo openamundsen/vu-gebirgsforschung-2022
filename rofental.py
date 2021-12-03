3e14b15f1dec07707d902d2a6ecbdf2d301b7869
Michael Warscher <michael.warscher@uibk.ac.at>
2021-12-03 15:36:35 +0100

    Update config and remove rofental.py

--- rofental.py
+++
@@ -1,8 +0,0 @@
-import os
-os.chdir("D:/setup_tirol/openamundsen-main")
-import openamundsen as oa
-
-config = oa.read_config('rofental/rofental.yml')
-model = oa.OpenAmundsen(config)
-model.initialize()
-model.run()
